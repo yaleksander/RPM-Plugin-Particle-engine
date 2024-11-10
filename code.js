206452b93d45d44fd848d7cc68022f04cc8ef777
Russo <aleksander@ice.ufjf.br>
2024-11-10 15:38:50 -0300

    not tested yet, hope this works

--- code.js
+++ code.js
@@ -3,6 +3,38 @@ import { THREE } from "../../System/Globals.js";
 
 const pluginName = "Particle effects";
 
+const clock = new THREE.Clock();
+const loader = new THREE.TextureLoader();
+const dummy = new THREE.Object3D();
+const emitterList = [];
+
+const vertShader = `
+attribute float instanceAlpha;
+
+varying float alpha;
+varying vec2 vUV;
+
+void main()
+{
+	//gl_Position = projectionMatrix * modelViewMatrix * instanceMatrix * vec4(position, 1.0);
+	gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+	vUV = vec3(uv, 1).xy;
+	alpha = instanceAlpha;
+}`;
+
+const fragShader = `
+uniform sampler2D diffuseTexture;
+
+varying float alpha;
+varying vec2 vUV;
+
+void main()
+{
+	vec4 tex = texture2D(diffuseTexture, vec2(vUV.x, vUV.y));
+	tex.a = alpha;
+	gl_FragColor = tex;
+}`;
+
 class Token
 {
 	static ADD   =  0;
@@ -22,6 +54,7 @@ class Token
 	static NUM   = 14;
 	static SQRT  = 15;
 	static POW   = 16;
+	static ROUND = 17;
 }
 
 function tokenize(text)
@@ -33,14 +66,11 @@ function tokenize(text)
 		text = text.replace("\t", "");
 	while (text.search("\n") !== -1)
 		text = text.replace("\n", "");
-	text.replace("-", "-1*");
 	const expr = [];
 	for (var i = 0; i < text.length; i++)
 	{
 		if (text[i] == '+')
 			expr.push({ token: Token.ADD, type: "op", value: null });
-		else if (text[i] == '-')
-			expr.push({ token: Token.SUB, type: "op", value: null });
 		else if (text[i] == '*')
 		{
 			if (text[++i] == '*')
@@ -130,11 +160,34 @@ function tokenize(text)
 			}
 		}
 		else if (text[i] == 'r')
-			expr.push({ token: Token.RAND, type: "val", value: null });
+		{
+			if (text[++i] == 'o')
+			{
+				if (text[++i] == 'u')
+				{
+					if (text[++i] == 'n')
+					{
+						if (text[++i] == 'd')
+							expr.push({ token: Token.ROUND, type: "func", value: null });
+						else
+							return null;
+					}
+					else
+						return null;
+				}
+				else
+					return null;
+			}
+			else
+			{
+				expr.push({ token: Token.RAND, type: "val", value: null });
+				i--;
+			}
+		}
 		else
 		{
 			var num = "";
-			while (num.search(/^\d*(\.\d*)?$/) === 0 && i < text.length)
+			while (num.search(/^-?\d*(\.\d*)?$/) === 0 && i < text.length)
 				num += text[i++].toString();
 			if (i < text.length)
 			{
@@ -143,7 +196,12 @@ function tokenize(text)
 			}
 			i--;
 			if (num.length > 0)
-				expr.push({ token: Token.NUM, type: "val", value: parseFloat(num) });
+			{
+				if (num == '-')
+					expr.push({ token: Token.SUB, type: "op", value: null });
+				else
+					expr.push({ token: Token.NUM, type: "val", value: parseFloat(num) });
+			}
 			else
 				return null;
 		}
@@ -231,17 +289,123 @@ function buildTree(expr)
 	return tree[0];
 }
 
-function evaluate(expr)
+function evaluate(expr, t, r)
 {
-	
+	switch (expr.token)
+	{
+		case Token.ADD:   return evaluate(expr.left, t, r) + evaluate(expr.right, t, r);
+		case Token.SUB:   return evaluate(expr.left, t, r) - evaluate(expr.right, t, r);
+		case Token.MULT:  return evaluate(expr.left, t, r) * evaluate(expr.right, t, r);
+		case Token.DIV:   return evaluate(expr.left, t, r) / evaluate(expr.right, t, r);
+		case Token.MOD:   return evaluate(expr.left, t, r) % evaluate(expr.right, t, r);
+		case Token.POW:   return Math.pow(evaluate(expr.left, t, r), evaluate(expr.right, t, r));
+		case Token.SQRT:  return Math.sqrt(evaluate(expr.left, t, r));
+		case Token.LN:    return Math.log(evaluate(expr.left, t, r));
+		case Token.SIN:   return Math.sin(evaluate(expr.left, t, r));
+		case Token.COS:   return Math.cos(evaluate(expr.left, t, r));
+		case Token.TAN:   return Math.tan(evaluate(expr.left, t, r));
+		case Token.ABS:   return Math.abs(evaluate(expr.left, t, r));
+		case Token.ROUND: return Math.round(evaluate(expr.left, t, r));
+		case Token.TIME:  return t;
+		case Token.RAND:  return r;
+		case Token.NUM:   return expr.value;
+		default:          return null;
+	}
 }
+
+setInterval(function ()
+{
+	if (RPM.Manager.Stack.top instanceof RPM.Scene.Map && !RPM.Scene.Map.current.loading)
+	{
+		const delta = clock.getDelta();
+		for (var i = 0; i < emitterList.length; i++)
+		{
+			const e = emitterList[i];
+			if (e.map === RPM.Scene.Map.current && !!e.mesh.parent)
+			{
+				e.emissionTime += delta;
+				if (e.rate > 0 && e.emissionTime > e.nextEmission)
+				{
+					e.nextEmission = e.emissionTime + (1 + Math.random()) / (2 * e.rate);
+					e.particles.push({ time: 0, rand: Math.random() });
+				}
+				for (var j = 0; j < e.maxParticles; j++)
+				{
+					if (j < e.particles.length)
+					{
+						const p = e.particles[j];
+						p.time += delta;
+						dummy.position.set(evaluate(e.px, p.time, p.rand), evaluate(e.py, p.time, p.rand), evaluate(e.pz, p.time, p.rand)).multiplyScalar(RPM.Datas.Systems.SQUARE_SIZE);
+						dummy.scale.set(1, 1, 1).multiplyScalar(size);
+						e.instanceAlpha[j] = evaluate(e.opacity, p.time, p.rand);
+						dummy.updateMatrix();
+						e.mesh.setMatrixAt(j, dummy.matrix);
+					}
+					else
+					{
+						dummy.position.set(0, 0, 0);
+						dummy.scale.set(0, 0, 0);
+						dummy.updateMatrix();
+						e.mesh.setMatrixAt(j, dummy.matrix);
+					}
+				}
+				e.mesh.geometry.setAttribute("instanceAlpha", new THREE.InstancedBufferAttribute(e.instanceAlpha, 1));
+				e.mesh.geometry.attributes.instanceAlpha.needsUpdate = true;
+				e.mesh.instanceMatrix.needsUpdate = true;
+			}
+			else
+				emitterList.splice(i--, 1);
+		}
+	}
+}, 16);
 
 RPM.Manager.Plugins.registerCommand(pluginName, "Start particle effect", (object, rate, lifespan, position, size, opacity, texture, additiveBlending) =>
 {
+	RPM.Core.MapObject.search(object, (result) =>
+	{
+		const tex = loader.load(texture);
+		const maxParticles = rate * lifespan;
+		const geo = new THREE.PlaneGeometry(tex.image.width, tex.image.height);
+		const mat = new THREE.ShaderMaterial({ uniforms: { diffuseTexture: tex }});
+		const mesh = new THREE.InstancedMesh(geo, mat, maxParticles);
+		result.object.mesh.add(mesh);
+		position = position.split(";");
+		emitterList.push(
+		{
+			id: object,
+			emissionTime: 0,
+			nextEmission: 0,
+			maxParticles: maxParticles,
+			map: RPM.Scene.Map.current,
+			instanceAlpha: new Float32Array(maxParticles),
+			mesh: mesh,
+			particles: [],
+			blending: additiveBlending ? THREE.AdditiveBlending : THREE.NormalBlending,
+			rate: rate,
+			lifespan: lifespan,
+			px: tokenize(position[0]),
+			py: tokenize(position[1]),
+			pz: tokenize(position[2]),
+			size: tokenize(size),
+			opacity: tokenize(opacity)
+		});
+	}, RPM.Core.ReactionInterpreter.currentObject);
 	console.log(opacity, tokenize(opacity));
 });
 
 RPM.Manager.Plugins.registerCommand(pluginName, "End particle effect", (object, smooth) =>
 {
-	
+	for (var i = 0; i < emitterList.length; i++)
+	{
+		const e = emitterList[i];
+		if (e.id === object)
+		{
+			e.rate = 0;
+			setTimeout(function ()
+			{
+				e.mesh.parent.remove(e.mesh);
+			}, smooth ? e.lifespan * 1000 : 1);
+			break;
+		}
+	}
 });
